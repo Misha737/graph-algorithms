788f77c3f3d0f04481414ea9bb4ab4e43f5f24dd
OBudiak <obudiak@kse.org.ua>
2025-04-11 13:07:39 +0300

    few changes

--- Graph/MatrixGraph.cs
+++ Graph/MatrixGraph.cs
@@ -1,11 +1,59 @@
 namespace Graph;
 
-// public class MatrixGraph<T> : BaseGraph<T> where T : IEquatable<T>
-// {
-//     private T[,] _data;
-//
-//     public MatrixGraph() : base()
-//     {
-//         
-//     }
-// }
+public class MatrixGraph<T> : BaseGraph<T> where T : IEquatable<T>
+{
+    private int[,] _data;
+    private List<T>? _edges;
+    private int _vertCount;
+
+    public MatrixGraph() : base()
+    {
+        
+    }
+    
+    public override void AddVertex(T vertex, List<T>? edges = null)
+    {
+        if (edges == null)
+        {
+            return;
+        }
+
+        if (edges.Contains(vertex))
+        {
+            AddEdges(vertex, edges);
+            return;
+        }
+        _edges!.Add(vertex);
+        _vertCount++;
+        int[,] newMatrix = new int[_vertCount, _vertCount];
+        for (int i = 0; i < _vertCount; i++)
+        {
+            for (int j = 0; j < _vertCount; j++)
+            {
+                if (i == _vertCount - 1 || j == _vertCount - 1)
+                {
+                    newMatrix[i, j] = 0;
+                    continue;
+                }
+
+                newMatrix[i, j] = _data[i, j];
+            }
+        }
+        _data = newMatrix;
+
+        AddEdges(vertex, edges);
+        
+    }
+    
+    public void AddEdges(T vertex, List<T>? edges)
+    {
+        if (edges == null)
+        {
+            return;
+        }
+        int vertIndex = edges.IndexOf(vertex);
+        foreach (var edge in edges)
+            _data[vertIndex, _edges!.IndexOf(edge)] = 1;
+        }
+    }
+}
