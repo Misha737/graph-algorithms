aa11f289c7700d923d35719816404223af387fd2
akoshelieva <akoshelieva@kse.org.ua>
2025-04-13 11:43:21 +0300

    fix generator

--- Analysis/Program.cs
+++ Analysis/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Graph;
 
-Console.WriteLine("Hello, World!");
+for (int n = 20; n <= 500; n += 20)
+{
+    for (int density = 10; density <= 90; density += 20)
+    {
+        var gen = new GraphGenerator();
+        var g = gen.GenerateMatrixGraph(n, density);
+
+        var sw = System.Diagnostics.Stopwatch.StartNew();
+        TarjanSCC.Run(g);
+        sw.Stop();
+
+        Console.WriteLine($"n = {n}, density = {density}, time = {sw.ElapsedMilliseconds} ms");
+    }
+}
--- Graph/GraphGenerator.cs
+++ Graph/GraphGenerator.cs
@@ -3,38 +3,27 @@
 public class GraphGenerator
 {
     private Random _rand = new();
-    public int? VertexCount;
-    public int? DensityCount;
-
-    public GraphGenerator(int vertex, int density)
-    {
-        VertexCount = vertex;
-        DensityCount = density;
-    }
 
     public MatrixGraph<int>? GenerateMatrixGraph(int vertexCount, int densityCount)
     {
-        if (vertexCount <= 0 || densityCount <= 0)
+        if (vertexCount <= 0 || densityCount < 0)
             return null;
+
         var graph = new MatrixGraph<int>();
-        List<int> vertexes = new();
-        int edgesDensity = vertexCount*vertexCount*(densityCount/100);
-        if (edgesDensity <= 0)
-            return null;
-        for (int i = 0; i < vertexCount; i++)
-        {
-            vertexes.Add(i);
-        }
-        graph.SetEmptyMatrix(vertexes);
+        List<int> vertexes = new List<int>();
 
         for (int i = 0; i < vertexCount; i++)
-        {
+            vertexes.Add(i);
+
+        graph.SetEmptyMatrix(vertexes);
+
+        double probability = densityCount / 100.0;
+
+        for (int i = 0; i < vertexCount; i++)
             for (int j = 0; j < vertexCount; j++)
-            {
-                graph.Matrix[i, j] = _rand.NextDouble() < edgesDensity ? 1 : 0;
-            }
-        }
-        
+                if (i != j && _rand.NextDouble() < probability)
+                    graph.Matrix[i, j] = 1;
+
         return graph;
     }
 }
